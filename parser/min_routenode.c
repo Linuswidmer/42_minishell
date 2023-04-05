3f41327ccb38206f53264efa588087b708fc89e9
Jonathan Strotbek <jstrotbe@c4c6c2.42berlin.de>
2023-04-05 21:13:50 +0200

    parser 1.1.6.1

--- parser/min_routenode.c
+++ parser/min_routenode.c
@@ -6,7 +6,7 @@
 /*   By: jstrotbe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 19:43:03 by jstrotbe          #+#    #+#             */
-/*   Updated: 2023/04/05 20:32:32 by jstrotbe         ###   ########.fr       */
+/*   Updated: 2023/04/05 21:13:31 by jstrotbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,33 @@
 
 
 #include "parser.h"
-t_ast	*ft_close_para(t_ast *ast)
-{
-		if (
-
-
-
-
+t_ast	*ft_close_para(t_ast *ast, char first)
+{		
+			
+		if (!ast)
+			return (NULL);
+		if (ast->key == jobnode)
+			ast = ast->node.job->up;
+		else if (ast->key == pipe && ast->node.pipe->prev)
+			ast = ast->node.pipe->prev;
+		else if (ast->key == pipe && !ast->node.pipe->prev)
+			ast = ast->node.pipe->up;
+		/* check if && route*/
+		else if (ast->key == route && ast->node.route->prev
+			ast = ast->node.route->prev;
+		else if (ast->key == route && ast->node.route->rvalue is && || ;)
+			 ast = 	ast->node.route->up;
+		else if (ast->key == route && ast->node.route->rvalue is () && !first)
+			return (ast);
+		else if (ast->key == route && ast->node.route->rvalue is () && first)
+			ast =  ast->node.route->up;
+		ft_close_para(ast, 0);
+}
 
 t_lexer	*min_routenode(t_lexer *token, t_ast **ast)
 {
 	if (min_token_is_para == 2)
-		*ast = ft_close_para(*ast);		
+		*ast = ft_close_para(*ast, 1);		
 	else if (min_token_is_para == 1)
 
 	else
