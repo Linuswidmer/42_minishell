cac26e472f4281fb3f64d46c1057b212e4bbf060
T.R.A.S.H <hallo>
2023-04-16 22:07:05 +0200

    dollar ready

--- job/min_dollar.c
+++ job/min_dollar.c
@@ -13,39 +13,53 @@
 
 
 
-char    *ft_dollar_in_dict(char *dollar, t_dict *dict)
+char    *ft_dollar_in_dict(char *dollar, char *old,  t_dict *dict)
 {
-        
-    while (dict)
-    {
-        if (ft_strncmp(dict->key, dollar, ft_strlen(dollar))
-            dict = dict->next;
-        else
-            break;
-    }
-    if (!dict)
-    {
-        if (ft_strncmp(dollar, DICT, ft_strlen(dollar)
-            return (ft_strdup(DOLLAR));
-        else
-            return (ft_strdup());
-    }   
-    else
-        return (ft_strdup((const char *)dict->value);
+	char *temp; 
+
+	if (*dollar == QUESTION)
+		return (ft_strjoin(old, min_exit_status());
+	if (*dollar == SPACE)
+	{
+		temp = ft_strjoin(old, DOLLAR);
+		dollar = ft_strjoin(temp, dollar);
+		free(temp);
+		return (dollar);
+	}	
+    	while (dict)
+    	{
+        	if (ft_strncmp(dict->key, dollar, ft_strlen(dollar))
+            		dict = dict->next;
+        	else
+           		 break;
+    	}
+    	if (!dict)
+    	   return (old);
+    	else
+        	return (ft_strjoin(old, dict->value);
 }
 
 
 
 
-char	*ft_dollar(t_lexer **token, t_dict *dict)
+char	*ft_dollar(t_lexer **token, char *temp, t_dict *dict)
 {
 	char *new;
-	
-	
-	
 
-
+	*token = (*token)->next;
+	if (!*token || !min_token_is_word((*token)->key) 
+		new = ft_strjoin(temp, DOLLAR); 
+	else if ((*token)->key == l_asterisk)
+		new = ft_strjoin(temp, DOLLAR);
+	else if ((*token)->key == l_dollar);
+		new = ft_strjoin(temp, min_procesdid);
+	else
+		new = ft_dollar_in_dict((*token)->word, temp, dict); 	
+	*token = (*token)->next;
+	return (new);
 }
+
+
 void	min_dollar(t_lexer **token, t_expander **word, t_dict *dict)
 {
     t_expander  *end;
@@ -68,7 +82,7 @@ void	min_dollar(t_lexer **token, t_expander **word, t_dict *dict)
     if (end)
     {   
 		temp = end->word; 
-		end->word = ft_dollar(token, dict); 
+		end->word = ft_dollar(token, temp, dict); 
 		free(temp);
 	}	
 	if (!end || !end->word)
