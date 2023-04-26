5142f8f7d03cd8425c117071adec6234f0bfd1c7
Linus Widmer <lwidmer@4b0c17.42berlin.de>
2023-04-26 20:09:16 +0200

    restructuring lexer parser

--- src_lexer/lexer_parser.c
+++ src_lexer/lexer_parser.c
@@ -79,18 +79,13 @@ int parse_dollar(char *input, int pos, t_lexer *tmp)
 empty quotes are not working yet!!! again :((
 */
 
-int parse_quote(char *input, int pos, t_lexer *tmp, t_lexertype token)
+static int quote_find_next_token_pos(char *input, int pos, t_lexertype token)
 {
-	t_lexertype next_token;
-	t_lexertype key;
-	int i;
-	int len;
-	int start;
 	int dollar_flag;
+	int i;
+	t_lexertype next_token;
 
-	start = pos;
 	i = 0;
-	dollar_flag = 0;
 	while (input[pos] != '\0')
 	{
 		next_token = check_token(input[pos], input[pos + 1]);
@@ -101,7 +96,40 @@ int parse_quote(char *input, int pos, t_lexer *tmp, t_lexertype token)
 		if (token == l_dquote && next_token == l_dollar)
 			dollar_flag = 1;
 	}
+	return (pos);
+}
+
+static int check_if_dollar_occurrs_in_dquote(char *input, int start, int pos, t_lexertype token)
+{
+	t_lexertype next_token;
+	int dollar_flag;
+	int i;
+	
+	i = 0;
+	dollar_flag = 0;
+	while (i < pos)
+	{
+		next_token = check_token(input[start + i], input[start + 1 + i]);
+		if (token == l_dquote && next_token == l_dollar)
+			dollar_flag = 1;
+		i++;
+	}
+	return (dollar_flag);
+}
+
+int parse_quote(char *input, int pos, t_lexer *tmp, t_lexertype token)
+{
+	t_lexertype key;
+	int i;
+	int len;
+	int start;
+	int dollar_flag;
+
+	start = pos;
+	i = 0;
+	pos = quote_find_next_token_pos(input, pos, token);
 	len = pos - start;
+	dollar_flag = check_if_dollar_occurrs_in_dquote(input, start, pos, token);
 	if (dollar_flag == 1)
 		key = l_dollar;
 	else
