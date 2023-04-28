8cb0bce56d17cfdbdb13f1a13f395c1273a7d81d
Linus Widmer <lwidmer@4b0c17.42berlin.de>
2023-04-28 12:05:14 +0200

    skdfhd

--- src/main.c
+++ src/main.c
@@ -14,68 +14,6 @@
 
 int min_status = 0;
 
-int test_builtins(t_min *min)
-{
-	int status;
-	char **args_export;
-	char **args_cd;
-	char **args_pwd;
-	char **args_env;
-	char **args_echo;
-	char **args_exit;
-	char **args_unset;
-
-	args_export = malloc(sizeof(char *) *1);
-    args_export[0] = NULL;
-    min->builtins[0].func(&(min->builtins[0]), args_export);
-	free(args_export);
-
-	/*
-	status = 0;
-    args_cd = malloc(sizeof(char *) *2);
-    args_cd[0] = ft_strdup("-");
-    args_cd[1] = NULL;  
-    min->builtins[1].func(&(min->builtins[1]), args_cd);
-	*/
-
-    /*
-    args_pwd = malloc(sizeof(char *) * 1);
-    args_pwd[0] = NULL;
-    min->builtins[2].func(&(min->builtins[2]), args_pwd);
-    */
-
-    /* 
-    args_env = malloc(sizeof(char *) * 1);
-    args_env[0] = NULL;
-    min->builtins[3].func(&(min->builtins[3]), args_env);
-    */
-
-    /*
-    args_echo = malloc(sizeof(char *) * 3);
-    args_echo[0] = ft_strdup("-n");
-    args_echo[1] = ft_strdup("world");
-    args_echo[2] = NULL;
-    min->builtins[5].func(&(min->builtins[5]), args_echo);
-    */
-
-    /*
-    args_exit = malloc(sizeof(char *) * 2);
-    args_exit[0] = ft_strdup("22");
-    args_exit[1] = NULL;
-    status = min->builtins[4].func(&(min->builtins[4]), args_exit);
-    printf("status: %i", status);   
-    */
-	args_unset = malloc(sizeof(char *) * 2);
-    args_unset[0] = ft_strdup("HOME");
-    args_unset[1] = NULL;
-    //min->builtins[6].func(&(min->builtins[6]), args_unset);
-
-    min->builtins[0].func(&(min->builtins[0]), args_export);
-
-	return (status);
-}
-
-
 int readline_loop(t_min *min)
 {
 	char *readline_input;
